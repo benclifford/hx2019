d84935c33b60b9c64a807a7ad9329b907a5c7c10
Ben Clifford <benc@hawaga.org.uk>
2019-10-09 15:46:45 +0000

    4. File metadata
    Now we're going to show more information about each file,
    in addition to each name.
    
    Now, a string isn't going to be enough to hold that information
    so we'll make a new data type to hold the information.
    
    part of this -- but just the size bit.
    need to add in sorting and _isdir later...
    
    * new datatype
    * ugly printing

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,28 @@
 module Main where
 
 import Data.Foldable as F
+import Data.Traversable as T
 import System.Directory as D
+
+data Fileinfo = Fileinfo {
+  _filename :: FilePath, -- aka String
+  _size :: Integer,
+  _isdir :: Bool
+}
 
 main :: IO ()
 main = do
   files <- D.listDirectory "."
-  F.for_ files putStrLn
+  files' <- elaborateFiles files
+  F.for_ files' putFileinfoLn
+
+elaborateFiles :: [FilePath] -> IO [Fileinfo]
+elaborateFiles files = do
+  T.for files $ \file -> 
+    Fileinfo file <$> D.getFileSize file <*> D.doesDirectoryExist file
+
+putFileinfoLn :: Fileinfo -> IO ()
+putFileinfoLn = putStrLn . renderFileinfo
+
+renderFileinfo :: Fileinfo -> String
+renderFileinfo file = _filename file ++ ", " ++ show (_size file) ++ " bytes"
